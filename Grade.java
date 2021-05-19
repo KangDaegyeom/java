ce877150cfc8073a0b77c43567edb8f190989c80
KangDaegyeom <75467449+KangDaegyeom@users.noreply.github.com>
2021-05-19 23:02:32 +0900

    Add files via upload

---
+++ Grade.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Grade {
+    private int math;
+    private int science;
+    private int english;
+
+    public Grade(int a, int b, int c) {
+        math = a;
+        science = b;
+        english = c;
+    }
+
+    public int average(){
+        return (math + science + english)/3;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("수학, 과학, 영어 순으로 3개의 점수 입력>>");
+        int math = scanner.nextInt();
+        int science = scanner.nextInt();
+        int english = scanner.nextInt();
+        Grade me = new Grade(math, science, english);
+        System.out.println("평균은 " + me.average());
+
+        scanner.close();
+    }
+}
